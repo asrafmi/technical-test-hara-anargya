5197812976cddcc082761d3defe64b61195d824e
asrafmi <asraf.muhammad07@gmail.com>
2024-02-08 20:35:46 +0700

    feat: add logic sort province data

---
+++ src/data/overview.js
@@ -0,0 +1,16 @@
+export const tableHead = [
+  {
+    field: 'id',
+    label: 'ID',
+    scope: 'col',
+    class: 'text-[#7C7C7C] normal-case px-6 py-5 w-[10%]',
+    sortable: true,
+  },
+  {
+    field: 'name',
+    label: 'Name',
+    scope: 'col',
+    class: 'text-[#7C7C7C] normal-case px-6 py-5 w-[80%]',
+    sortable: true,
+  },
+];
