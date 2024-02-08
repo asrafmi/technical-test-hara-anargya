a7d66c12dd3f6b6f597fe0d27b0d7b2b1bb5e38e
asrafmi <asraf.muhammad07@gmail.com>
2024-02-08 23:36:01 +0700

    feat: make reusable input

--- src/pages/Overview/components/ProvinceTable.vue
+++ src/pages/Overview/components/ProvinceTable.vue
@@ -1,41 +1,10 @@
 <template>
   <div class="relative overflow-x-auto shadow-md rounded-lg">
-    <form class="absolute top-1 right-5">
-      <label
-        for="default-search"
-        class="mb-2 text-sm font-medium text-gray-900 sr-only"
-        >Search</label
-      >
-      <div class="relative flex items-center">
-        <div
-          class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
-        >
-          <svg
-            class="w-4 h-4 text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Search Province by City ID"
-          v-model="cityID"
-          @input="findProvinceByCityID(cityID)"
-        />
-      </div>
-    </form>
+    <SearchInput
+      placeholder="Search Province by City ID"
+      :value="cityID"
+      @update:value="handleSearch"
+    />
     <table class="w-full text-sm text-left text-[#080808]">
       <caption
         class="p-5 text-lg font-semibold text-left text-[#080808] bg-white"
@@ -93,6 +62,7 @@
 <script setup>
   import { ref } from 'vue';
   import { Icon } from '@iconify/vue';
+  import SearchInput from '../../../components/SearchInput.vue';
   import { tableHead } from '../../../data/overview';
   import provinceData from '../../../assets/js/province';
   import cityData from '../../../assets/js/city';
@@ -139,6 +109,11 @@
       }
       return 0;
     });
+  };
+
+  const handleSearch = (value) => {
+    cityID.value = value;
+    findProvinceByCityID(value);
   };
 
   const findProvinceByCityID = (id) => {
